d7665387256f3d7e7178e7048e2f7dfdea98086b
worldyone <amanohikari142857@gmail.com>
2019-11-23 21:30:17 +0900

    内包表記の確認

--- temp/hello.py
+++ temp/hello.py
@@ -1,6 +1,34 @@
-print("Hello, world!")
+#print("Hello, world!")
 
 for i in range(10):
     if i == 5:
-        print(input())
-    print(i)
+        #print(input())
+    #print(i)
+        pass
+
+li = [input() for _ in range(5)]
+
+#print(li)
+
+#print("----")
+#print("\n".join(li))
+
+print("----")
+ll = [l for l in li if print(l)]
+
+print(ll)
+print("----")
+
+lg = [l for l in li]
+#print(lg)
+
+if print("hee"):
+    print("Yes!")
+
+#print(print("dkdk"))
+
+lll = [list(map(lambda x: (x,print(x)), [l])) for l in li]
+#print(lll)
+
+la = [l for l in li if not print(l)]
+print(la)
