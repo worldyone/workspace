16efd014e271a3ad63e1e219fbd80f363f423f83
worldyone <amanohikari142857@gmail.com>
2020-11-18 23:56:13 +0900

    paiza D080 js

---
+++ paiza/D080.js
@@ -0,0 +1,15 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  var line = lines[0].split(" ");
+  console.log(line[0] * 6000 + line[1] * 4000);
+});
