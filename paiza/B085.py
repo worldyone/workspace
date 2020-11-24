731e68ddce82d88c2f91f28ed21adcc6114f0c03
worldyone <amanohikari142857@gmail.com>
2020-11-25 00:16:24 +0900

    paiza D085 clear

--- paiza/B085.py
+++ paiza/B085.py
@@ -1,7 +1,8 @@
 N = int(input())
 d1_idx = abs(N) * 2 - int(N > 0)
 
-d2_bin_len = len(bin(N - int(N > 0))) - 2
+d2_bin_len = len(bin(N + [1, -1][int(N > 0)])) - \
+    2 if abs(N) != 1 else (-N + abs(N)) // 2
 d2_idx = d2_bin_len + 2
 
 d1 = d1_idx * (d1_idx + 1) // 2
@@ -13,7 +14,3 @@ else:
     d2 = 0
 
 print(d1, d2)
-
-print(d2_bin_len)
-print(d2_idx)
-print(bin(N))
