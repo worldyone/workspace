782547ffc5a288edd8302e8a3ed7c4de7e3e220c
worldyone <amanohikari142857@gmail.com>
2021-01-06 23:54:30 +0900

    paiza C018

---
+++ paiza/C018.py
@@ -0,0 +1,20 @@
+
+recipe_cnt = int(input())
+recipes = {}
+for _ in range(recipe_cnt):
+    lines = input().split()
+    a, b = lines[0], int(lines[1])
+    recipes[a] = b
+
+m_cnt = int(input())
+m = {}
+for _ in range(m_cnt):
+    lines = input().split()
+    a, b = lines[0], int(lines[1])
+    m[a] = b
+
+ans = 0
+while(True):
+    for
+print(recipes)
+print(m)
