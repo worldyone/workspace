323d3f1b0a39710052c52a19beb372c526797634
worldyone <amanohikari142857@gmail.com>
2021-01-25 21:42:28 +0900

    マウスクリックのイベントの理解

---
+++ pygames/tutorial/circle_onclick.py
@@ -0,0 +1,32 @@
+import sys
+import pygame
+from pygame.locals import QUIT, MOUSEBUTTONDOWN
+
+pygame.init()
+SURFACE = pygame.display.set_mode((400, 300))
+FPSCLOCK = pygame.time.Clock()
+
+
+def main():
+    """ main routine """
+    mousepos = []
+
+    while True:
+        SURFACE.fill((255, 255, 255))
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == MOUSEBUTTONDOWN:
+                mousepos.append(event.pos)
+
+        for i, j in mousepos:
+            pygame.draw.circle(SURFACE, (0, 255, 0), (i, j), 5)
+
+        pygame.display.update()
+        FPSCLOCK.tick(10)
+
+
+if __name__ == '__main__':
+    main()
