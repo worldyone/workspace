afdb9ce5ad661ca8f18279373f3925aeead4f948
worldyone <amanohikari142857@gmail.com>
2021-01-25 18:41:13 +0900

    円、楕円、線の描画

--- pygames/tutorial/draw_rect1.py
+++ pygames/tutorial/draw_rect1.py
@@ -6,6 +6,7 @@ pygame.init()
 SURFACE = pygame.display.set_mode((400, 300))
 FPSCLOCK = pygame.time.Clock()
 
+
 def main():
     """main routine"""
 
@@ -27,9 +28,23 @@ def main():
         rect1 = Rect((30, 160), (100, 50))
         pygame.draw.rect(SURFACE, (255, 255, 0), rect1)
 
+        # circle( Surface, color, pos, radius, width = 0) -> Rect
+        pygame.draw.circle(SURFACE, (255, 0, 0), (150, 50), 20, 10)
+
+        # ellipse( Surface, color, Rect, width = 0) -> Rect
+        pygame.draw.ellipse(SURFACE, (0, 255, 0), (50, 150, 110, 60), 5)
+
+        # line( Surface, color, start_ pos, end_ pos, width = 1) -> Rect
+        # 青： 斜線（ 太 さ 10）
+        start_pos = (300, 30)
+        end_pos = (380, 200)
+        pygame.draw.line(SURFACE, (0, 0, 255), start_pos, end_pos, 10)
+
         pygame.display.update()
         FPSCLOCK.tick(3)
 
 
 if __name__ == '__main__':
+    print(sys.version)
+    print(sys.path)
     main()
