5f30b76e8949becba03f35d7a72f6bb1a521526c
worldyone <amanohikari142857@gmail.com>
2021-11-12 23:43:04 +0900

    statelesswidgetの利用

--- FlutterProject/myapp/lib/main.dart
+++ FlutterProject/myapp/lib/main.dart
@@ -1,5 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Text('Practice', textDirection: TextDirection.ltr));
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _todos = List.generate(
+      10,
+      (index) => 'Todo ${index + 1}',
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ToDo'),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) => ListTile(
+          title: Text(_todos[index]),
+        ),
+        itemCount: _todos.length,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          _todos.add('ToDo ${_todos.length + 1}');
+        },
+      ),
+    );
+  }
 }
