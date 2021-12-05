338f354ed96a1a610bb2bdec0e84f57737ee20b0
worldyone <amanohikari142857@gmail.com>
2021-12-05 22:10:57 +0900

    時間の表示

--- FlutterProject/kitchentimer/lib/main.dart
+++ FlutterProject/kitchentimer/lib/main.dart
@@ -1,4 +1,8 @@
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_picker/flutter_picker.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -11,36 +15,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'キッチンタイマー',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'キッチンタイマー'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,68 +33,71 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String _timeStr = '';
+  DateTime _time = DateTime(0, 0, 0, 0, 3, 0, 0, 0); // 3分
+  final DateFormat formatter = DateFormat('mm:ss');
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    Timer.periodic(
+      const Duration(seconds: 1),
+      _onTimer,
+    );
+    super.initState();
+  }
+
+  void _onTimer(Timer timer) {
+    _time = _time.add(const Duration(seconds: -1));
+    setState(() => _timeStr = formatter.format(_time));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              '残り時間',
             ),
             Text(
-              '$_counter',
+              _timeStr,
               style: Theme.of(context).textTheme.headline4,
+            ),
+            FloatingActionButton(
+              onPressed: () {},
+              tooltip: 'Increment',
+              child: const Icon(
+                Icons.arrow_right_rounded,
+                size: 48.0,
+              ),
+            ),
+            TextButton(
+              child: const Text('edit',
+                  style: TextStyle(decoration: TextDecoration.underline)),
+              onPressed: () async {
+                Picker(
+                  adapter: DateTimePickerAdapter(
+                      type: PickerDateTimeType.kHMS,
+                      value: _time,
+                      customColumnType: [3, 4, 5]),
+                  title: const Text("Select Time"),
+                  onConfirm: (Picker picker, List value) {
+                    setState(() => {
+                          _time = DateTime.utc(
+                              0, 0, 0, value[0], value[1], value[2])
+                        });
+                  },
+                ).showModal(context);
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
