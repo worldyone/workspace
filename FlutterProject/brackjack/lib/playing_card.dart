d5b03a1027e0610b1294c1fbf7326ca49a3e1c58
worldyone <amanohikari142857@gmail.com>
2022-01-08 13:02:27 +0900

    impl CardNumberのgetter add PlayingCardクラスにimageを追加

--- FlutterProject/brackjack/lib/playing_card.dart
+++ FlutterProject/brackjack/lib/playing_card.dart
@@ -23,15 +23,37 @@ enum CardNumber {
   king,
 }
 
+extension CardNumberEx on CardNumber {
+  static Map<CardNumber, int> cardNumber2decimal = {
+    CardNumber.ace: 1,
+    CardNumber.two: 2,
+    CardNumber.three: 3,
+    CardNumber.four: 4,
+    CardNumber.five: 5,
+    CardNumber.six: 6,
+    CardNumber.seven: 7,
+    CardNumber.eight: 8,
+    CardNumber.nine: 9,
+    CardNumber.ten: 10,
+    CardNumber.jack: 11,
+    CardNumber.queen: 12,
+    CardNumber.king: 13,
+  };
+  int? get decimal => cardNumber2decimal[this];
+  String get zfillDecimal =>
+      cardNumber2decimal[this].toString().padLeft(2, '0');
+}
+
 class PlayingCard {
   CardSuit suit;
   CardNumber number;
-  Image? image;
+  Image image;
   bool? opened;
 
   PlayingCard({
     required this.suit,
     required this.number,
+    required this.image,
     this.opened,
   });
 }
