f0cdbda7725d01a622a205b3927b729b77293ea4
worldyone <amanohikari142857@gmail.com>
2022-02-16 19:42:31 +0900

    ブロックにボールがぶつかったら、ステージのブロック総数を一つ減らし、ブロックを削除

--- Unity/BlockBreak/Assets/Scripts/Block.cs
+++ Unity/BlockBreak/Assets/Scripts/Block.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+
+    /// ぶつかり処理
+    // ぶつかるのはボールとのみ
+    // ステージのブロック総数を1つ減らし、ブロックが消える.
     void OnCollisionEnter(Collision other)
     {
+        GameObject factory = GameObject.Find("BlockFactory");
+        BlockFactory factoryComponent = factory.GetComponent<BlockFactory>();
+        factoryComponent.BlockNum--;
+
         Destroy(gameObject);
     }
 }
