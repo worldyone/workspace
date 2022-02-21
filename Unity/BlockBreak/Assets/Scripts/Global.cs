d338373273756e538999fb7a44cc9c0b582b1c37
worldyone <amanohikari142857@gmail.com>
2022-02-21 22:17:39 +0900

    パネルに属性を付与して色変更

--- Unity/BlockBreak/Assets/Scripts/Global.cs
+++ Unity/BlockBreak/Assets/Scripts/Global.cs
@@ -3,4 +3,11 @@ public class Global
     public static float OUT_OF_FIELD = -30.0f;
     public static float LEFT_WALL_X = -15.0f;
     public static float RIGHT_WALL_X = 15.0f;
+
+    public enum PanelAttribute
+    {
+        Fire,
+        Water,
+        Earth,
+    }
 }
--- Unity/BlockBreak/Assets/Scripts/Panel.cs
+++ Unity/BlockBreak/Assets/Scripts/Panel.cs
@@ -1,9 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Global;
 
 public class Panel : MonoBehaviour
 {
+    void Start()
+    {
+        // 属性をランダムに付与
+        int no = UnityEngine.Random.Range(0, Enum.GetValues(typeof(PanelAttribute)).Length);
+        PanelAttribute attribute = (PanelAttribute)Enum.ToObject(typeof(PanelAttribute), no);
+
+        switch (attribute)
+        {
+            case PanelAttribute.Fire:
+                // 赤色に変更する
+                gameObject.GetComponent<Renderer>().material.color = Color.red;
+                break;
+            case PanelAttribute.Water:
+                // 水色に変更する
+                gameObject.GetComponent<Renderer>().material.color = Color.blue;
+                break;
+            case PanelAttribute.Earth:
+                // 黄色に変更する
+                gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+                break;
+        }
+    }
 
     void Update()
     {
@@ -14,16 +38,15 @@ public class Panel : MonoBehaviour
         }
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter(Collider other)
     {
-        string name = LayerMask.LayerToName(collision.gameObject.layer);
+        string name = LayerMask.LayerToName(other.gameObject.layer);
 
         // パドルと当たったら、取得処理
         if (name == "Paddle")
         {
             Destroy(gameObject);
         }
-
     }
 
 }
