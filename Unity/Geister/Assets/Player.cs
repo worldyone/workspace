57518ecfd9d0b7ce71d35bfc29f65b0f6c07d650
worldyone <amanohikari142857@gmail.com>
2022-05-20 22:31:09 +0900

    Playerクラスの作成

---
+++ Unity/Geister/Assets/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    public bool IsPlayer;
+    public int PlayerNo;
+
+    // ゴールタイルに乗っているかどうか
+    public bool IsGoal;
+    // 相手の赤いコマを取ったらHP減少
+    public int Hp = 4;
+    // 相手の青いコマを取ったら得点
+    public int Score;
+
+    // プレイヤーの名前を返す
+    public string GetPlayerName()
+    {
+        string ret = "";
+
+        string playerName = PlayerNo + "P";
+
+        if (!IsPlayer)
+        {
+            playerName = "CPU";
+        }
+
+        ret = playerName;
+        return ret;
+    }
+
+}
