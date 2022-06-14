bd4c54b502a194e5a2ade6545c0b1673e87e939a
worldyone <amanohikari142857@gmail.com>
2022-06-14 23:04:02 +0900

    C#演習

---
+++ Unity/NewProjectStudioShimazu/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public Text HPText;
+    public Slider HPSlider;
+    Transform tf;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = GetComponent<Transform>();
+        HPText.text = "50";
+        HPSlider.value = 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tf.position += new Vector3(1f, 0f, 0f);
+    }
+}
