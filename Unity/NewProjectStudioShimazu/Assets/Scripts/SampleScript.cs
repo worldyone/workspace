45067e1d27547bda92f033ffee3052052cf2ca9c
worldyone <amanohikari142857@gmail.com>
2022-06-24 23:18:31 +0900

    シーンの移動

---
+++ Unity/NewProjectStudioShimazu/Assets/Scripts/SampleScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SampleScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
