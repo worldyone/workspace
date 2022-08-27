251fc3865b1dd82022f9b1d31eac69c106556296
worldyone <amanohikari142857@gmail.com>
2022-08-27 13:38:55 +0900

    appear: 探索中はボタンを表示しない

--- Unity/NonFieldRPGForUdemy/Assets/Scripts/Quest/QuestManager.cs
+++ Unity/NonFieldRPGForUdemy/Assets/Scripts/Quest/QuestManager.cs
@@ -12,7 +12,7 @@ public class QuestManager : MonoBehaviour
     public GameObject questBG;
 
     // 敵に遭遇するテーブル：-1なら遭遇しない, 0なら遭遇
-    int[] encounterTable = { -1, -1, 0, -1, 0, -1 };
+    int[] encounterTable = { -1, -1, 0, -1, 0, -1, -1, -1, -1 };
     int currentStage = 0;
 
     void Start()
@@ -20,15 +20,18 @@ public class QuestManager : MonoBehaviour
         stageUI.UpdateUI(currentStage);
     }
 
-    public void OnNextButton()
+    IEnumerator Explore()
     {
+        // 背景を大きくする
         questBG.transform.DOScale(new Vector3(1.5f, 1.5f, 1.5f), 2f)
             .OnComplete(() => questBG.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f));
+        // フェードアウト
         SpriteRenderer questBGSpriteRenderer = questBG.GetComponent<SpriteRenderer>();
         questBGSpriteRenderer.DOFade(0, 2f)
             .OnComplete(() => questBGSpriteRenderer.DOFade(1, 0f));
 
-        SoundManager.instance.PlaySE(0);
+        // 演出が終わるまで2秒間待機
+        yield return new WaitForSeconds(2f);
 
         currentStage++;
         stageUI.UpdateUI(currentStage);
@@ -43,6 +46,17 @@ public class QuestManager : MonoBehaviour
             Debug.Log("敵に遭遇した！");
             EncounterEnemy();
         }
+        else
+        {
+            stageUI.ShowButtons(true);
+        }
+    }
+
+    public void OnNextButton()
+    {
+        SoundManager.instance.PlaySE(0);
+        stageUI.ShowButtons(false);
+        StartCoroutine(Explore());
     }
 
     public void OnToTownButton()
