bb95d713a44d4cc212ca9264b01d9d51ab4f5cc1
worldyone <amanohikari142857@gmail.com>
2022-09-02 09:13:02 +0900

    feat: カード移動の実装

---
+++ Unity/CardGameForUdemy/Assets/Scripts/CardMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CardMovement : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    public Transform defaultParent;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        defaultParent = transform.parent;
+        transform.SetParent(defaultParent.parent, false);
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(defaultParent, false);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+}
