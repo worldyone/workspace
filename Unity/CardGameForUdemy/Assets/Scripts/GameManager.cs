0bf6be80d11d69ec517d5e3fb93c6088f9ad13c0
worldyone <amanohikari142857@gmail.com>
2022-09-02 12:02:59 +0900

    feat: ターンの実装

--- Unity/CardGameForUdemy/Assets/Scripts/GameManager.cs
+++ Unity/CardGameForUdemy/Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,16 +6,63 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] Transform playerHandTransform;
+    [SerializeField] Transform enemyHandTransform;
     [SerializeField] CardController cardPrefab;
+    bool isPlayerTurn;
 
     void Start()
     {
-        CreateCard(playerHandTransform);
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        SettingInitHand();
+        isPlayerTurn = true;
+        TurnCalc();
+    }
+
+    void SettingInitHand()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            CreateCard(playerHandTransform);
+            CreateCard(enemyHandTransform);
+        }
+    }
+
+    void TurnCalc()
+    {
+        if (isPlayerTurn)
+        {
+            PlayerTurn();
+        }
+        else
+        {
+            EnemyTurn();
+        }
+    }
+
+    public void ChangeTurn()
+    {
+        isPlayerTurn = !isPlayerTurn;
+        TurnCalc();
+    }
+
+    void PlayerTurn()
+    {
+        Debug.Log("playerのターン");
+    }
+
+    void EnemyTurn()
+    {
+        Debug.Log("enemyのターン");
+        ChangeTurn();
     }
 
     void CreateCard(Transform hand)
     {
-        CardController card = Instantiate(cardPrefab, playerHandTransform, false);
+        CardController card = Instantiate(cardPrefab, hand, false);
         card.Init(2);
     }
 
